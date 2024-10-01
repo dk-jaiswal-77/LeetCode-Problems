400290a55ded4bacb23066c399b0458bd48cf91d
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2024-10-02 04:23:30 +0530

    Time: 198 ms (9.71%) | Memory: 123.3 MB (41.23%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -12,18 +12,25 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         string forward = "";
-        string backward = "";
+        // string backward = "";
         // long long place = 1;
         ListNode* temp = head;
         char val;
         while(temp != NULL) {
             val = (char)(temp -> val);
             forward.push_back(val);
-            backward.insert(0, 1, val);
+            // backward.insert(0, 1, val);
             // place *= 10;
             temp = temp -> next;
         }
-        if (forward == backward) return true;
-        return false;
+
+        int left = 0;
+        int right = forward.size() - 1;
+        while (left < right) {
+            if (forward[left] != forward[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 };
