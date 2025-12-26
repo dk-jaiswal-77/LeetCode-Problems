b9817ce747bbdce06892eda9aac04d3560441882
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2025-12-26 10:50:13 +0530

    Time: 1 ms (96.75%) | Memory: 46.2 MB (64.66%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,27 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        HashMap<Integer, Integer> inorderMappings = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMappings.put(inorder[i], i);
+        }
+        return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1, inorderMappings);
     }
 
-    private TreeNode build(int[] preorder, int[] inorder, int pL, int pR, int iL, int iR) {
+    private TreeNode build(int[] preorder, int[] inorder, int pL, int pR, int iL, int iR, HashMap<Integer, Integer> inorderMappings) {
         if (iL > iR) return null;
         if (iL == iR) {
             return new TreeNode(inorder[iL]);
         }
         TreeNode root = new TreeNode(preorder[pL]);
-        int rootIdx = indexOf(root.val, inorder);
+        int rootIdx = inorderMappings.get(root.val);
         int lCount = rootIdx - iL;
-        root.left = build(preorder, inorder, pL + 1, pL + lCount, iL, rootIdx-1);
-        root.right = build(preorder, inorder, pL + lCount + 1, pR, rootIdx + 1, iR);
+        root.left = build(preorder, inorder, pL + 1, pL + lCount, iL, rootIdx-1, inorderMappings);
+        root.right = build(preorder, inorder, pL + lCount + 1, pR, rootIdx + 1, iR, inorderMappings);
         return root;
-    }
-
-    private int indexOf(int ele, int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == ele) return i;
-        }
-        return -1;
     }
 }
