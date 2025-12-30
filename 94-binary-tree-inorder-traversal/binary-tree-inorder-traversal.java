abcb6cb8d3ecbbe51d9d345fa02b60899ce3b0fc
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2025-12-30 18:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (82.95%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void createThread(TreeNode root, TreeNode toLeftOf) {
+        TreeNode curr = root;
+        // traverse to the rightmost node of the subtree
+        while (curr.right != null) curr = curr.right;
+        curr.right = toLeftOf;
+    }
+
+    private boolean doesThreadExists(TreeNode root, TreeNode toLeftOf) {
+        // traverse till the rightmost node of subtree
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.right == toLeftOf) return true;
+            curr = curr.right;
+        }
+        return false;
+    }
+
+    private void cutThread(TreeNode root, TreeNode toLeftOf) {
+        TreeNode curr = root;
+        while (curr.right != toLeftOf) curr = curr.right;
+        curr.right = null;
+    }
+
+    private void inTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        // case1: left subtree is null
+        if (root.left == null) {
+            res.add(root.val);
+            inTraversal(root.right, res);
+            return;
+        }
+        // case2: left subtree exists
+        // check if left part traversed or not
+        boolean threadExists = doesThreadExists(root.left, root);
+        if (threadExists) {
+            // thread exists --> traversed. so cut the thread and traverse right
+            cutThread(root.left, root);
+            res.add(root.val);
+            inTraversal(root.right, res);
+            return;
+        }
+
+        // thread does not exist --> yet to be traversed
+        // create thread & traverse left
+        createThread(root.left, root);
+        // findLeftSubTreeLastNode(root.left).right = root;
+        inTraversal(root.left, res);
+    }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inTraversal(root, res);
+        return res;
+    }
+}
