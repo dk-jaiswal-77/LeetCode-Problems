cecf00612c266ecb2c468a7d7f9bcf9f16cc1025
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2026-01-03 22:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (33.07%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -14,15 +14,17 @@
  * }
  */
 class Solution {
-    private boolean findAndDel(TreeNode curr, TreeNode parent, boolean isLeft, int key) {
-        if (curr == null) return false;
+    private void findAndDel(TreeNode curr, TreeNode parent, boolean isLeft, int key) {
+        if (curr == null) return;
         if (curr.val == key) {
             if (isLeft) parent.left = delete(curr);
             else parent.right = delete(curr);
-            return true;
+            return;
         }
-        if (findAndDel(curr.left, curr, true, key)) return true;
-        return findAndDel(curr.right, curr, false, key);
+        if (curr.val < key) findAndDel(curr.right, curr, false, key);
+        else findAndDel(curr.left, curr, true, key);
+        // if (findAndDel(curr.left, curr, true, key)) return true;
+        // return findAndDel(curr.right, curr, false, key);
     }
     
     public TreeNode deleteNode(TreeNode root, int key) {
@@ -30,8 +32,10 @@ class Solution {
         if (root.val == key) {
             return delete(root);
         }
-        boolean found = findAndDel(root.left, root, true, key);
-        if (!found) findAndDel(root.right, root, false, key);
+        // boolean found = findAndDel(root.left, root, true, key);
+        // if (!found) findAndDel(root.right, root, false, key);
+        if (root.val < key) findAndDel(root.right, root, false, key);
+        else findAndDel(root.left, root, true, key);
         return root;
     }
 
