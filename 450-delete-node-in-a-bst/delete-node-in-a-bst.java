8b37126f047e9a14c531d86d15896a110a1e5834
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2026-01-03 21:46:54 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (55.20%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean findAndDel(TreeNode curr, TreeNode parent, boolean isLeft, int key) {
+        if (curr == null) return false;
+        if (curr.val == key) {
+            if (isLeft) parent.left = delete(curr);
+            else parent.right = delete(curr);
+            return true;
+        }
+        if (findAndDel(curr.left, curr, true, key)) return true;
+        return findAndDel(curr.right, curr, false, key);
+    }
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+        if (root.val == key) {
+            return delete(root);
+        }
+        boolean found = findAndDel(root.left, root, true, key);
+        if (!found) findAndDel(root.right, root, false, key);
+        return root;
+    }
+
+    private TreeNode delete(TreeNode root) {
+        if (root.left == null && root.right == null) return null;
+        if (root.left == null) return root.right;
+        if (root.right == null) return root.left;
+        // travel to the rightmost node of the left subtree && attach root.right to it
+        TreeNode curr = root.left;
+        while (curr.right != null) curr = curr.right;
+        curr.right = root.right;
+        return root.left;
+    }
+}
