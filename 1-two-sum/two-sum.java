cd5977108be212509b30da82822930979834edfe
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2026-01-05 11:15:16 +0530

    Time: 35 ms (43.17%) | Memory: 47.3 MB (10.35%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,6 +7,7 @@ class Solution {
                 if (nums[i] + nums[j] == target) {
                     res[0] = i;
                     res[1] = j;
+                    break;
                 }
             }
         }
