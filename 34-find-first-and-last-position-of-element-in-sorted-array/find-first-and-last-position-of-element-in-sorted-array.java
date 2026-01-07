0c82a5115a16e1c7b96b5f88a2ff75a3e3164578
Dhirendra Kumar Jaiswal <92107053+dk-jaiswal-77@users.noreply.github.com>
2026-01-07 12:18:46 +0530

    Time: 1 ms (17.28%) | Memory: 48.4 MB (13.67%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,40 @@
 class Solution {
-    public static int firstOccurenceIdx(int[] arr, int t) {
-        int left = 0;
-        int right = arr.length - 1;
-        int mid;
-        int idx = -1;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (arr[mid] == t) {
-                idx = mid;
-                right = mid - 1;
-            } else if (arr[mid] < t) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return idx;
-    }
-    public static int lastOccurenceIdx(int[] arr, int t) {
-        int left = 0;
-        int right = arr.length - 1;
-        int mid;
-        int idx = -1;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (arr[mid] == t) {
-                idx = mid;
-                left = mid + 1;
-            } else if (arr[mid] < t) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return idx;
-    }
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-        int foidx = firstOccurenceIdx(nums, target);
-        if (foidx >= 0) {
-            res[0] = foidx;
-            res[1] = lastOccurenceIdx(nums, target);
+        int first = firstPos(nums, target);
+        if (first < 0) return new int[]{-1, -1};
+        int last = lastPos(nums, target);
+        return new int[]{first, last};
+    }
+    private int firstPos(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        int idx = -1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                idx = mid;
+                right = mid - 1;
+            }
+            else if (target < nums[mid]) right = mid - 1;
+            else left++;
         }
-        return res;
+        return idx;
+    }
+
+    private int lastPos(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        int idx = -1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                idx = mid;
+                left = mid + 1;
+            } else if (target > nums[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+        return idx;
     }
 }
